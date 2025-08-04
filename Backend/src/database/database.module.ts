18360aeeb2370fed40a8d6c2fe473671245f44bf
Tayyab Kamran Sami <tayyabsammy1@gmail.com>
2025-08-04 19:59:01 +0500

    chore: db module basic

---
+++ Backend/src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigService } from "@nestjs/config";
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.get('DB_HOST'),
+                port: parseInt(config.get('DB_PORT') || '5432'),
+                username: config.get('DB_USERNAME'),
+                password: config.get('DB_PASSWORD'),
+                database: config.get('DB_NAME'),
+                autoLoadEntities: true,
+                synchronize: true, // Set to false in production
+            }),
+    ]
+    ,
+    controllers: [],
+    providers: [],
+    exports: [],
+    // Add any database-specific providers or configurations here
+
+})
+export class DatabaseModule { }
